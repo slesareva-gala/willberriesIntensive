fc634a357b39753423d8e8d97464c360a29ea16d
Gala Slesareva <67021657+slesareva-gala@users.noreply.github.com>
2021-11-19 17:49:11 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,11 @@
+﻿
+const search = ()=> {
+  const input = document.querySelector('.search-block > input')
+  const searchBtn = document.querySelector('.search-block > button')
+
+  searchBtn.addEventListener('click', ( event )=>{
+    console.log(input.value);
+  })
+
+}
+search()
