2e8658b97752e9824753b62d2b0626271ed8447d
Gala Slesareva <67021657+slesareva-gala@users.noreply.github.com>
2021-11-20 13:30:12 +0300

    Add files via upload

---
+++ js/getGoods.js
@@ -0,0 +1,19 @@
+﻿
+const getGoods = ()=> {
+  const links = document.querySelectorAll(".navigation-link")
+
+  const getData = () => {
+    fetch("https://goods-9a67e-default-rtdb.firebaseio.com/db.json")
+      .then( (res) => res.json() )
+      .then( (data) => localStorage.setItem('goods', JSON.stringify(data)) )
+  }
+
+  links.forEach( (link) =>{
+    link.addEventListener('click', (event)=>{
+      event.preventDefault()
+      getData()
+    } )
+  } )
+}
+
+getGoods()
